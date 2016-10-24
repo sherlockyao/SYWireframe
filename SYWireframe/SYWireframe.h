14bb1c8432151c02b89d6ac5f0f84163cbebd563
Sherlock Yao <yaosherlock@gmail.com>
2016-10-24 19:52:54 +0800

    init project

---
+++ SYWireframe/SYWireframe.h
@@ -0,0 +1,19 @@
+//
+//  SYWireframe.h
+//  SYWireframe
+//
+//  Created by SherlockYao on 10/24/16.
+//  Copyright © 2016 SherlockYao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SYWireframe.
+FOUNDATION_EXPORT double SYWireframeVersionNumber;
+
+//! Project version string for SYWireframe.
+FOUNDATION_EXPORT const unsigned char SYWireframeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SYWireframe/PublicHeader.h>
+
+
